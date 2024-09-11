7d210c2fc0f9d968a74f4291aa27b170b7796c17
Jadala Karthikeya <jadalakarthikeya@gmail.com>
2024-09-11 21:39:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ document.getElementById('registrationForm').addEventListener('submit', function(
 
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
     const dob = document.getElementById('dob').value;
-   
-    const li = document.createElement('li');
-    li.textContent = `Name: ${name}, Email: ${email}, Password: ${password}, date of Birth: ${dob}`;
-    
-    document.getElementById('userList').appendChild(li);
+    const password = document.getElementById('password').value;
+
+
+    const tableRow = document.createElement('tr');
+
+ 
+    tableRow.innerHTML = `
+        <td>${name}</td>
+        <td>${email}</td>
+        <td>${dob}</td>
+        <td>${password}</td>
+    `;
+
+  
+    document.getElementById('userTableBody').appendChild(tableRow);
+
 
     document.getElementById('registrationForm').reset();
 });
